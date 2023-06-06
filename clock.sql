ea69576c6da56fdd1f0dcc5e65fc95e72333fb72
a1357422 <a1357422@gmail.com>
2023-06-06 13:09:37 +0800

    2023/06/06 資料庫備份

--- clock.sql
+++ clock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1:3306
--- 產生時間： 2023-06-06 02:09:20
+-- 產生時間： 2023-06-06 05:09:03
 -- 伺服器版本： 8.0.31
 -- PHP 版本： 8.0.26
 
@@ -52,11 +52,11 @@ INSERT INTO `basesalary` (`id`, `basesalary`, `created_at`, `updated_at`) VALUES
 DROP TABLE IF EXISTS `failed_jobs`;
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `failed_jobs` (
 DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE IF NOT EXISTS `migrations` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -96,8 +96,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 DROP TABLE IF EXISTS `password_resets`;
 CREATE TABLE IF NOT EXISTS `password_resets` (
-  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -111,11 +111,11 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 DROP TABLE IF EXISTS `personal_access_tokens`;
 CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tokenable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_type` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `tokenable_id` bigint UNSIGNED NOT NULL,
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -133,17 +133,17 @@ CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
 DROP TABLE IF EXISTS `punchrecord`;
 CREATE TABLE IF NOT EXISTS `punchrecord` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '編號',
-  `date` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '月份',
+  `date` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '月份',
   `nameid` bigint UNSIGNED NOT NULL COMMENT '工讀生編號',
-  `punch_in` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '上班打卡時間',
-  `punch_out` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '下班打卡時間',
-  `time` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '時數',
+  `punch_in` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '上班打卡時間',
+  `punch_out` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '下班打卡時間',
+  `time` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '時數',
   `mark` tinyint(1) DEFAULT NULL COMMENT '更改',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `punchrecord_nameid_foreign` (`nameid`)
-) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 傾印資料表的資料 `punchrecord`
@@ -165,7 +165,10 @@ INSERT INTO `punchrecord` (`id`, `date`, `nameid`, `punch_in`, `punch_out`, `tim
 (42, '6/5', 2, '15:03', '17:00', '1時57分', NULL, '2023-06-05 07:03:56', '2023-06-05 09:00:21'),
 (44, '6/5', 8, '16:43', '17:40', '0時57分', NULL, '2023-06-05 08:43:34', '2023-06-05 09:40:38'),
 (45, '6/5', 10, '16:43', '17:41', '0時58分', NULL, '2023-06-05 08:43:38', '2023-06-05 09:41:41'),
-(46, '6/5', 12, '16:45', '17:40', '0時55分', NULL, '2023-06-05 08:45:17', '2023-06-05 09:40:58');
+(46, '6/5', 12, '16:45', '17:40', '0時55分', NULL, '2023-06-05 08:45:17', '2023-06-05 09:40:58'),
+(47, '6/6', 10, '11:59', '13:01', '1時0分', NULL, '2023-06-06 03:59:57', '2023-06-06 05:01:19'),
+(49, '6/6', 9, '12:19', '13:01', '0時42分', NULL, '2023-06-06 04:19:43', '2023-06-06 05:01:14'),
+(50, '6/6', 6, '12:05', '13:02', '1時0分', 1, '2023-06-06 04:22:24', '2023-06-06 05:07:40');
 
 -- --------------------------------------------------------
 
@@ -176,13 +179,13 @@ INSERT INTO `punchrecord` (`id`, `date`, `nameid`, `punch_in`, `punch_out`, `tim
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '編號',
-  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '身份',
-  `username` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `cardID` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '卡號',
-  `studentID` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '學號',
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '身份',
+  `username` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cardID` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '卡號',
+  `studentID` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '學號',
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -199,7 +202,7 @@ INSERT INTO `users` (`id`, `name`, `role`, `username`, `password`, `cardID`, `st
 (3, '劉彥芝', NULL, NULL, NULL, '1216331429', 'D1094421701', NULL, '2023-06-02 02:58:27', '2023-06-02 04:18:46'),
 (4, '蔡定辰', NULL, NULL, NULL, NULL, 'D1094161033', NULL, '2023-06-02 02:58:47', '2023-06-02 02:58:47'),
 (5, '許肇恒', NULL, NULL, NULL, NULL, 'D1094161032', NULL, '2023-06-02 02:59:00', '2023-06-02 02:59:00'),
-(6, '王鈺欣', NULL, NULL, NULL, NULL, 'D1104442702', NULL, '2023-06-02 02:59:29', '2023-06-02 02:59:29'),
+(6, '王鈺欣', NULL, NULL, NULL, '1098286645', 'D1104442702', NULL, '2023-06-02 02:59:29', '2023-06-06 05:03:54'),
 (7, '謝承祐', NULL, NULL, NULL, NULL, 'D1104111708', NULL, '2023-06-02 02:59:39', '2023-06-02 02:59:39'),
 (8, '劉伃芹', NULL, NULL, NULL, '1098412693', 'D1104431013', NULL, '2023-06-02 02:59:49', '2023-06-05 08:43:26'),
 (9, '謝宜軒', NULL, NULL, NULL, '1147044437', 'D1104431034', NULL, '2023-06-02 03:00:08', '2023-06-02 08:44:32'),
