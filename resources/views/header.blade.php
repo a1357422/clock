d4a863d59dcc14328bafa482e5b61c281b19e756
a1357422 <a1357422@gmail.com>
2023-06-08 11:29:46 +0800

    修正

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,20 +2,6 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <script>
-    function confirmAction() {
-      // 使用 confirm 函數顯示確認對話框
-      var result = confirm("確定要同步線上資料庫嗎?這將覆蓋掉目前本機資料！");
-      
-      // 如果用戶點擊了確定按鈕
-      if (result) {
-        // 在這裡執行您想要的動作
-        window.location.href = "/downloaddb";
-      } else {
-        // 用戶取消操作，不執行任何動作
-      }
-    }
-  </script>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
         @guest
@@ -27,9 +13,6 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/users">工讀生資料</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" onclick="confirmAction(); return false;" >同步連線版資料</a>
         </li>
         @if (Route::has('login'))
           <li class="nav-item">
@@ -45,9 +28,6 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/basesalary/1/edit">修改基本薪資</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" onclick="confirmAction(); return false;" >同步連線版資料</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
