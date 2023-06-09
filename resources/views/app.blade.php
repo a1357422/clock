c8948212bd18cfa0e35eae16d166a1d8f9b35553
a1357422 <a1357422@gmail.com>
2023-06-09 19:27:12 +0800

    若使用手機開啟打卡系統，導回校網

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,6 +20,7 @@
     // 在這裡插入上述的JavaScript程式碼
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         confirm("請使用電腦開啟");
+        window.location.href = "https://www.lhu.edu.tw/index2.asp";
     // 檢測到手機瀏覽器
     // 在這裡執行相應的操作，例如重新導向或顯示錯誤訊息
     }
