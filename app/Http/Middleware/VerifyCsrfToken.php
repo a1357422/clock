6e91b801e32f6a8719364d6ae6cb4c5ec5c0ca24
a1357422 <a1357422@gmail.com>
2023-06-17 12:27:24 +0800

    測試csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -11,7 +12,21 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
+    /**
+    * Handle an incoming request.
+    *
+    * @param \Illuminate\Http\Request $request
+    * @param \Closure $next
+    * @return mixed
+    */
     protected $except = [
         //
     ];
+    public function handle($request, Closure $next)
+    {
+    // 使用CSRF
+    //return parent::handle($request, $next);
+    // 禁用CSRF
+        return $next($request);
+    }
 }
