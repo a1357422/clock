b5ba042f73138386f7f8fc9398d83632dd917e23
ChihHao <a1357422@gmail.com>
2024-06-24 08:18:45 +0800

    家機上線

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,8 @@
 <hr>
 <h6 style="text-align: center;" class="no-print">龍華科技大學 衛保組打卡系統<br>
     校園電話：(02)82093211
-    衛保組分機:#3360-3362
     系統維修：a1357422@gmail.com
+    維修電話：0907-845-515 😎
+    <img src="{{ asset('icon/weini.png') }}" alt="" style="vertical-align: middle; width: 70px; height: 70px;">
 </h6>
 </div>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,20 +6,20 @@
     function confirmAction() {
       // 使用 confirm 函數顯示確認對話框
 
-      var currentTime = new Date();  // 建立一個Date物件，表示現在的時間
-      var currentMonth = currentTime.getUTCMonth();  // 建立一個Date物件，表示現在的時間
-      var currentDay = currentTime.getDate();  // 建立一個Date物件，表示現在的時間
-      var currentDay1 = currentDay-1;  // 建立一個Date物件，表示現在的時間
-      var currentHour = currentTime.getHours();  // 獲取當前小時（0-23）
-      var currentMinute = currentTime.getMinutes();  // 獲取當前分鐘（0-59）
-      var currentDate = currentMonth+1 +"/"+currentDay1;
-      var result = confirm("確定要同步線上資料庫嗎?\n這將加載"+currentDate+" 18:00的資料並覆蓋掉目前本機資料！");
-      
+      var currentTime = new Date(); // 建立一個Date物件，表示現在的時間
+      var currentMonth = currentTime.getUTCMonth(); // 建立一個Date物件，表示現在的時間
+      var currentDay = currentTime.getDate(); // 建立一個Date物件，表示現在的時間
+      var currentDay1 = currentDay - 1; // 建立一個Date物件，表示現在的時間
+      var currentHour = currentTime.getHours(); // 獲取當前小時（0-23）
+      var currentMinute = currentTime.getMinutes(); // 獲取當前分鐘（0-59）
+      var currentDate = currentMonth + 1 + "/" + currentDay1;
+      var result = confirm("確定要同步線上資料庫嗎?\n這將加載" + currentDate + " 18:00的資料並覆蓋掉目前本機資料！");
+
       // 如果用戶點擊了確定按鈕
       if (result) {
         // 在這裡執行您想要的動作
         var userInput = prompt("請輸入後台管理者密碼：");
-        if (userInput == "82093211"){
+        if (userInput == "82093211") {
           window.location.href = "/downloaddb";
         }
       } else {
@@ -29,48 +29,48 @@
   </script>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-        @guest
-        <li class="nav-item">
-          <a class="nav-link" href="/punch/create">打卡</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/punch">打卡紀錄</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/users">工讀生資料</a>
-        </li>
-        <li class="nav-item">
+      @guest
+      <li class="nav-item">
+        <a class="nav-link" href="/punch/create">打卡</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/punch">打卡紀錄</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/users">工讀生資料</a>
+      </li>
+      <!-- <li class="nav-item">
           <a class="nav-link" href="#" onclick="confirmAction(); return false;" >同步連線版資料</a>
-        </li>
-        @if (Route::has('login'))
-          <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">{{ __('登入') }}</a>
-          </li>
-        @endif
-        @else
-        <li class="nav-item">
-          <a class="nav-link" href="/users">工讀生資料</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/punch/create">今日打卡紀錄</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/punch">工讀生打卡紀錄</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/basesalary/1/edit">修改基本薪資</a>
-        </li>
-        <li class="nav-item">
+        </li> -->
+      @if (Route::has('login'))
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">{{ __('登入') }}</a>
+      </li>
+      @endif
+      @else
+      <li class="nav-item">
+        <a class="nav-link" href="/users">工讀生資料</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/punch/create">今日打卡紀錄</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/punch">工讀生打卡紀錄</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/basesalary/1/edit">修改基本薪資</a>
+      </li>
+      <!-- <li class="nav-item">
           <a class="nav-link" href="#" onclick="confirmAction(); return false;" >同步連線版資料</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+        </li> -->
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-              {{ __('登出') }}
-          </a>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-              @csrf
-          </form>
-        </li>
-        @endguest
+          {{ __('登出') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
+      </li>
+      @endguest
   </div>
